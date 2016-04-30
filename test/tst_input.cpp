5d686b11e88a95152dbf1025f680923b267b9e6a
Andy Gocke <angocke@microsoft.com>
2016-04-30 12:01:53 -0700

    Refactor tst_input to avoid double macro expansion
    tst_input.cpp was failing to compile under VS2015 for me due to
    double macro expansion, as #ifdef's were placed inside Qt macros.

--- test/tst_input.cpp
+++ test/tst_input.cpp
@@ -4,40 +4,41 @@
 
 class TestInput: public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 private slots:
-	void specialKeys();
+    void specialKeys();
 private:
-	NeovimQt::InputConv input;
+    NeovimQt::InputConv input;
 };
 
 void TestInput::specialKeys()
 {
-	foreach(int k, input.specialKeys.keys()) {
-		QCOMPARE(input.convertKey("", k, Qt::NoModifier),
-			QString("<%1>").arg(input.specialKeys.value(k)));
+    foreach(int k, input.specialKeys.keys()) {
+        QCOMPARE(input.convertKey("", k, Qt::NoModifier),
+            QString("<%1>").arg(input.specialKeys.value(k)));
 
-		QCOMPARE(input.convertKey("", k, Qt::ControlModifier),
+
 #ifdef Q_OS_MAC
-			// On Mac Control is actually the Cmd key, which we
-			// don't support yet
-			QString("<D-%1>").arg(input.specialKeys.value(k)));
+            // On Mac Control is actually the Cmd key, which we
+            // don't support yet
+        auto ctrlMap = QString("<D-%1>").arg(input.specialKeys.value(k));
 #else
-			QString("<C-%1>").arg(input.specialKeys.value(k)));
+        auto ctrlMap = QString("<C-%1>").arg(input.specialKeys.value(k));
 #endif
+        QCOMPARE(input.convertKey("", k, Qt::ControlModifier), ctrlMap);
 
-		QCOMPARE(input.convertKey("", k, Qt::AltModifier),
-			QString("<A-%1>").arg(input.specialKeys.value(k)));
+        QCOMPARE(input.convertKey("", k, Qt::AltModifier),
+            QString("<A-%1>").arg(input.specialKeys.value(k)));
 
-		QCOMPARE(input.convertKey("", k, Qt::MetaModifier),
 #ifdef Q_OS_MAC
-			// On Mac Meta is actually the Control key
-			QString("<C-%1>").arg(input.specialKeys.value(k)));
+        // On Mac Meta is actually the Control key
+        auto metaMap = QString("<C-%1>").arg(input.specialKeys.value(k));
 #else
-			// Meta is not handled right now
-			QString("<%1>").arg(input.specialKeys.value(k)));
+        // Meta is not handled right now
+        auto metaMap = QString("<%1>").arg(input.specialKeys.value(k));
 #endif
-	}
+        QCOMPARE(input.convertKey("", k, Qt::MetaModifier), metaMap);
+    }
 }
 
 QTEST_MAIN(TestInput)
